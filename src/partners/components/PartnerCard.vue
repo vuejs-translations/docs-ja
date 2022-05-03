9862e712b64895ef3e5f22bdd33a15d7b5e734cc
渡辺健五 <skmkids.kenken@gmail.com>
2022-05-03 14:58:35 +0900

    improve partners cssを取り込み

--- src/partners/components/PartnerCard.vue
+++ src/partners/components/PartnerCard.vue
@@ -2,16 +2,13 @@
 import { Partner } from './type'
 import { normalizeName, getHero, getLogo } from './utils'
 import Location from './PartnerLocation.vue'
-
 const { data, hero, page } = defineProps<{
   data: Partner
   hero?: boolean
   page?: boolean
 }>()
-
-const { name, intro, region, logo, proficiencies } = data
+const { name, intro, region, logo, proficiencies, flipLogo } = data
 </script>
-
 <template>
   <component
     :is="page ? 'div' : 'a'"
@@ -20,6 +17,26 @@ const { name, intro, region, logo, proficiencies } = data
     :href="'/partners/' + normalizeName(name) + '.html'"
   >
     <div class="info">
+
+    
+          
+            
+    
+
+          
+          
+            
+    
+
+          
+    
+    @@ -77,7 +77,7 @@ h3 {
+  
+      <img
+        class="logo dark"
+        v-if="hero && flipLogo"
+        :src="getLogo(logo, flipLogo)"
+      />
       <img class="logo" v-if="hero" :src="getLogo(logo)" />
       <h3 v-else>{{ name }}</h3>
       <p class="region"><Location /> {{ region.join(', ') }}</p>
@@ -29,10 +46,9 @@ const { name, intro, region, logo, proficiencies } = data
         <span class="proficiency" v-for="p in proficiencies">{{ p }}</span>
       </p>
     </div>
-    <img :src="getHero(name)" :alt="name + ' hero'" />
+    <img class="big" :src="getHero(name)" :alt="name + ' hero'" />
   </component>
 </template>
-
 <style scoped>
 .partner-card {
   background-color: var(--vt-c-bg);
@@ -47,46 +63,55 @@ const { name, intro, region, logo, proficiencies } = data
   transition: background-color 0.5s, box-shadow 0.25s ease,
     border-color 0.25s ease;
 }
-
 .partner-card.hero {
   font-size: 16px;
   flex-direction: row;
   width: 100%;
 }
-
 .partner-card.page {
   padding: 0;
 }
-
 h3 {
   font-size: 1.3em;
   font-weight: 700;
   letter-spacing: -0.1px;
   margin-bottom: 1em;
 }
-
 .logo {
   margin-bottom: 1em;
   max-width: 240px;
+  max-height: 120px;
+}
+.logo.dark,
+.flipLogo .dark .logo:not(.dark) {
+  display: none;
 }
 
-.dark .logo {
-  filter: grayscale(1) invert(1);
-}
+    
+          
+            
+    
 
-.partner-card:not(.hero) img {
+          
+    
+    
+  
+.dark .logo.dark {
+  display: inline-block;
+}
+.partner-card:not(.hero) .big {
   margin-top: auto;
 }
-
 .partner-card.hero .info {
   margin-right: 2em;
 }
-.partner-card.hero img {
+.partner-card.hero .big {
   display: inline-block;
   margin-left: auto;
   width: 60%;
+  max-height: 360px;
+  object-fit: cover;
 }
-
 @media (max-width: 768px) {
   .partner-card {
     width: 100%;
@@ -97,52 +122,42 @@ h3 {
   .logo {
     max-width: 200px;
   }
-  .partner-card.hero img {
+  .partner-card.hero .big {
     width: 100%;
   }
 }
-
 .partner-card:hover {
   box-shadow: 0 12px 12px rgba(0, 0, 0, 0.1);
 }
-
 .partner-card:hover h3 {
   color: var(--vt-c-green);
 }
-
 .partner-card h3 {
   transition: color 0.25s ease;
 }
-
 .dark .partner-card,
 .partner-card.page {
   box-shadow: none !important;
 }
-
 .dark .partner-card:not(.hero) {
   border: 1px solid var(--vt-c-divider-light);
 }
-
 .dark .partner-card:not(.hero):hover {
   border-color: #555;
 }
-
 .partner-card h3 {
   font-size: 1.5em;
   font-weight: 700;
   letter-spacing: -0.1px;
   margin-bottom: 0.4em;
 }
-
 .partner-card p {
   margin-bottom: 1.6em;
 }
-
 .region {
   color: var(--vt-c-text-2);
   font-size: 0.9em;
 }
-
 .region svg {
   display: inline-block;
   opacity: 0.5;
@@ -150,13 +165,11 @@ h3 {
   top: -1px;
   left: -2px;
 }
-
 h4 {
   font-size: 1.2em;
   font-weight: 600;
   margin-bottom: 0.6em;
 }
-
 .proficiency {
   color: var(--vt-c-text-code);
   font-weight: 600;
