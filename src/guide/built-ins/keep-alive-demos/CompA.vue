3cd3615cd8a6ec22288099acc9b54a823e48867f
TAKAHASHI Shuuji <shuuji3@gmail.com>
2022-05-10 09:48:28 +0000

    revert unneeded translations

--- src/guide/built-ins/keep-alive-demos/CompA.vue
+++ src/guide/built-ins/keep-alive-demos/CompA.vue
@@ -3,7 +3,7 @@ let count = $ref(0)
 </script>
 
 <template>
-  <p>現在のコンポーネント: A</p>
-  <span style="margin-right:20px">カウント: {{ count }}</span>
+  <p>Current component: A</p>
+  <span style="margin-right:20px">count: {{ count }}</span>
   <button @click="count++">+</button>
 </template>
--- src/guide/built-ins/keep-alive-demos/CompB.vue
+++ src/guide/built-ins/keep-alive-demos/CompB.vue
@@ -3,7 +3,7 @@ let msg = $ref('')
 </script>
 
 <template>
-  <p>現在のコンポーネント: B</p>
-  <span style="margin-right:20px;">メッセージ: {{ msg }}</span>
+  <p>Current component: B</p>
+  <span style="margin-right:20px;">Message is: {{ msg }}</span>
   <input v-model="msg">
 </template>
