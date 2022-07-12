8fb351ed89701f9a13bae22481a062315e57e992
Pierre Schweiger <schweiger.pierre@gmail.com>
2022-07-12 04:02:00 +0200

    Add vuemastery banner (#1825)

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,13 +2,13 @@ import './styles/index.css'
 import { h, App } from 'vue'
 import { VPTheme } from '@vue/theme'
 import PreferenceSwitch from './components/PreferenceSwitch.vue'
+import VueMasteryBanner from './components/VueMasteryBanner.vue'
 import {
   preferComposition,
   preferSFC,
   filterHeadersByPreference
 } from './components/preferences'
 import SponsorsAside from './components/SponsorsAside.vue'
-import VueSchoolLink from './components/VueSchoolLink.vue'
 import VueJobs from './components/VueJobs.vue'
 import VueSchoolLink from './components/VueSchoolLink.vue'
 
@@ -16,6 +16,9 @@ export default Object.assign({}, VPTheme, {
   Layout: () => {
     // @ts-ignore
     return h(VPTheme.Layout, null, {
+      banner: () => h('div', {}, [
+        h(VueMasteryBanner)
+      ]),
       'sidebar-top': () => h(PreferenceSwitch),
       'aside-mid': () => h(SponsorsAside),
       'aside-bottom': () => h(VueJobs)
@@ -25,6 +28,5 @@ export default Object.assign({}, VPTheme, {
     app.provide('prefer-composition', preferComposition)
     app.provide('prefer-sfc', preferSFC)
     app.provide('filter-headers', filterHeadersByPreference)
-    app.component('VueSchoolLink', VueSchoolLink)
   }
 })
