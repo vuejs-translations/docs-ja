ee8b2fe4e8ada50dac6dc6afd99155b68c0aee61
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2022-09-14 19:56:05 +0900

    copyright 修正

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -675,7 +675,7 @@ export default defineConfigWithTheme<ThemeConfig>({
         text: 'MIT ライセンス',
         link: 'https://opensource.org/licenses/MIT'
       },
-      copyright: `Copyright © 2014-${new Date().getFullYear()} Evan You`
+      copyright: '日本語ドキュメントの内容の著作権は Vue 公式チームと翻訳協力者にあり、すべての権利は留保されています。'
     }
   },
 
