36ce2f5ec9c589771b287b3372eed4b37c07a390
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2022-10-05 19:29:51 +0900

    「API > Options API > Options: Misc」の翻訳

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -383,7 +383,7 @@ export const sidebar = {
           text: 'Options: Composition',
           link: '/api/options-composition'
         },
-        { text: 'Options: Misc', link: '/api/options-misc' },
+        { text: 'オプション: その他', link: '/api/options-misc' },
         {
           text: 'Component Instance',
           link: '/api/component-instance'
