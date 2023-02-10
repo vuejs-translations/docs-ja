d6bfa0d93613bf6ef6b64b11135064cb2d7f342a
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2023-02-10 19:39:59 +0900

    「Ecosystem > Themes」の翻訳

--- src/ecosystem/themes/ThemeListItem.vue
+++ src/ecosystem/themes/ThemeListItem.vue
@@ -34,7 +34,7 @@ const description = computed(() => {
 
     <div class="action">
       <VTLink class="action-link" :href="provider.seeMoreUrl" no-icon>
-        See More Themes from {{ provider.name }}
+        {{ provider.name }} のテーマをもっと見る
       </VTLink>
     </div>
   </section>
