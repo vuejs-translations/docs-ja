47a1a344f395172bbdf36f9b40ba30e1169850d2
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2023-10-05 21:58:08 +0900

    Vue Fes Japan 2023 チケット広告（文言変更）

--- .vitepress/theme/components/Banner.vue
+++ .vitepress/theme/components/Banner.vue
@@ -23,7 +23,7 @@ function dismiss() {
 <template>
   <div class="banner" v-if="open">
     <a href="https://vuefes.jp/2023/#ticket" target="_blank">
-      Vue Fes Japan 2023 チケット発売中！ <span>9月30日（土）までのご購入で 1,000円 OFF!</span>
+      Vue Fes Japan 2023 チケット発売中！ <span>数に限りがあります。購入はお早めに</span>
     </a>
     <button @click="dismiss">
       <VTIconPlus class="close" />
