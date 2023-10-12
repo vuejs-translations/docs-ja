389c39246edb9da7c2425e004b910b197c53b4f4
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2023-10-12 19:10:02 +0900

    Italian translation ready to launch (#1585)

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -686,6 +686,11 @@ export default defineConfigWithTheme<ThemeConfig>({
         repo: 'https://github.com/vuejs-translations/docs-bn'
       },
       {
+        link: 'https://it.vuejs.org',
+        text: 'Italiano',
+        repo: 'https://github.com/vuejs-translations/docs-it'
+      },
+      {
         link: '/translations/',
         text: '翻訳にご協力ください！',
         isTranslationsDesc: true
