1e851a958892d2a55df9dcc550aa22b2e027e5a2
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2024-03-28 18:58:26 +0900

    docs: add Kevin Deng as core team member

--- src/about/team/TeamPage.vue
+++ src/about/team/TeamPage.vue
@@ -1,5 +1,8 @@
 <script lang="ts">
-const shuffleMembers = (members: Member[], pinTheFirstMember = false): void => {
+const shuffleMembers = (
+  members: Member[],
+  pinTheFirstMember = false
+): void => {
   let offset = pinTheFirstMember ? 1 : 0
   // `i` is between `1` and `length - offset`
   // `j` is between `0` and `length - offset - 1`
@@ -7,11 +10,8 @@ const shuffleMembers = (members: Member[], pinTheFirstMember = false): void => {
   // `offset + j` is between `offset` and `length - 1`
   let i = members.length - offset
   while (i > 0) {
-    const j = Math.floor(Math.random() * i);
-    [
-      members[offset + i - 1],
-      members[offset + j]
-    ] = [
+    const j = Math.floor(Math.random() * i)
+    ;[members[offset + i - 1], members[offset + j]] = [
       members[offset + j],
       members[offset + i - 1]
     ]
@@ -37,46 +37,47 @@ shuffleMembers(membersPartnerData as Member[])
   <div class="TeamPage">
     <TeamHero>
       <template #title>チーム紹介</template>
-      <template #lead
-        >Vue とそのエコシステムの開発は
-        国際チームによって主導されています。
-        一部のメンバーを以下に記載します。</template
-      >
+      <template #lead>
+        Vue とそのエコシステムの開発は
+        国際的なチームによって主導されています。
+        その一部のメンバーを以下に記載します。
+      </template>
 
       <template #action>
         <VTLink
           href="https://github.com/vuejs/governance/blob/master/Team-Charter.md"
-          >チームについてもっと知る</VTLink
         >
+          チームについてもっと知る
+        </VTLink>
       </template>
     </TeamHero>
 
-    <TeamList :members="membersCoreData as Member[]">
+    <TeamList :members="(membersCoreData as Member[])">
       <template #title>コアチームメンバー</template>
-      <template #lead
-        >コアチームメンバーは1つから複数のプロジェクトを
+      <template #lead>
+        コアチームメンバーは1つから複数のプロジェクトを
         積極的にメンテナンスしています。
         彼らは Vue エコシステムに有意な貢献をしており、
-        プロジェクトとユーザーの成功のために長い期間献身しています。</template
-      >
+        プロジェクトとユーザーの成功のために長い期間献身しています。
+      </template>
     </TeamList>
 
-    <TeamList :members="membersEmeritiData as Member[]">
+    <TeamList :members="(membersEmeritiData as Member[])">
       <template #title>名誉コアチーム</template>
-      <template #lead
-        >私達が誇る過去に価値のある貢献をした
-        元コアチームメンバーです。</template
-      >
+      <template #lead>
+        私達が誇る過去に価値のある貢献をした
+        元コアチームメンバーです。
+      </template>
     </TeamList>
 
-    <TeamList :members="membersPartnerData as Member[]">
+    <TeamList :members="(membersPartnerData as Member[])">
       <template #title>コミュニティーパートナー</template>
-      <template #lead
-        >一部の Vue コミュニティーメンバーは
+      <template #lead>
+        一部の Vue コミュニティーメンバーは
         コミュニティーを発展させたため、特筆します。
         私達は主要なパートナーとより親密に開発し、
-        頻繁に今後の機能やニュースを決定しています。</template
-      >
+        頻繁に今後の機能やニュースを決定しています。
+      </template>
     </TeamList>
   </div>
 </template>
