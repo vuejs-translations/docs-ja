a2100e289bf7302767ad89c6f2a908fb3a41fc45
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2024-06-10 22:27:55 +0900

    chore: bump deps

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -768,7 +768,6 @@ export default defineConfigWithTheme<ThemeConfig>({
   markdown: {
     theme: 'github-dark',
     config(md) {
-      // @ts-expect-error - broken type output in vitepress
       md.use(headerPlugin)
       // .use(textAdPlugin)
     }
