5d1543684f5ee86f5e300a7c22f60611f0f94d5e
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2024-09-25 00:21:43 +0900

    Add developers pages, Proxify as a partner (#2317)

--- src/partners/components/PartnerJoin.vue
+++ src/partners/components/PartnerJoin.vue
@@ -1,70 +1,12 @@
+<script setup lang="ts">
+import CallToActionSection from '@theme/components/CallToActionSection.vue'
+</script>
+
 <template>
-  <div class="divider"></div>
-  <div class="join-container">
-    <h2>Vue でビジネスを成長させる</h2>
-    <p>
-      新しいプロジェクトの優位性を生み出し、クライアントへの信頼を高め、
-      Vue.js とそのエコシステムの長期的な持続可能性を
-      サポートできます。
-    </p>
-    <a
-      class="join"
-      href="https://airtable.com/shrCQhat57SApJI2l"
-      target="_blank"
-      >パートナーになる</a
-    >
-  </div>
+  <CallToActionSection
+    title="Vue でビジネスを成長させる"
+    description="新しいプロジェクトの優位性を生み出し、クライアントへの信頼を高め、Vue.js とそのエコシステムの長期的な持続可能性をサポートできます。"
+    link="https://airtable.com/shrCQhat57SApJI2l"
+    linkText="パートナーになる"
+  />
 </template>
-
-<style scoped>
-.divider {
-  width: 100px;
-  margin: 0px auto;
-  border-top: 1px solid var(--vt-c-divider-light);
-}
-
-.join-container {
-  max-width: 688px;
-  padding: 28px 28px 96px;
-  text-align: center;
-  margin: 0 auto;
-}
-
-.lead {
-  color: var(--vt-c-text-2);
-  font-weight: 500;
-  font-size: 18px;
-}
-
-h2 {
-  font-weight: 600;
-  letter-spacing: -0.5px;
-  font-size: 34px;
-  line-height: 1.2;
-  margin: 0.5em 0 1em;
-}
-
-p {
-  color: var(--vt-c-text-2);
-}
-
-.join {
-  margin-top: 2em;
-  display: inline-block;
-  text-align: center;
-  background-color: var(--vt-c-bg-mute);
-  color: var(--vt-c-text-code);
-  padding: 12px 24px;
-  font-weight: 600;
-  border-radius: 6px;
-  transition: background-color 0.5s, color 0.5s;
-}
-
-.join:hover {
-  background-color: var(--vt-c-gray-light-4);
-}
-
-.dark .join:hover {
-  background-color: var(--vt-c-gray-dark-3);
-}
-</style>
